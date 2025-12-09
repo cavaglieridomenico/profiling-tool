fe3c760b5da5ad68bc2687c9dead0c0f24fb4eac
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2025-12-09 14:24:43 +0100

    change command names

--- src/testCases.js
+++ src/testCases.js
@@ -1,10 +1,10 @@
 const testCases = {
-  vmmv_tc01_tc04: [
+  vmmv_tc01__tc04: [
     { command: 'trace:start', delay: 2000 },
     { command: 'navigate:refresh', delay: 20000 },
     { command: 'trace:stop', delay: 0 },
   ],
-  vmmv_tc05_tc06: [
+  vmmv_tc05__tc06: [
     { command: 'trace:start', delay: 2000 },
     { command: 'input:tap-vmmv-multivm-open', delay: 13000 },
     { command: 'input:tap-vmmv-multivm-close', delay: 5000 },
@@ -29,17 +29,17 @@ const testCases = {
     { command: 'input:tap-vmcore-vmp-pdplight', delay: 10000 },
     { command: 'trace:stop', delay: 0 },
   ],
-  vmcore_tc20: [
+  vmcore_tc19_01: [
     { command: 'input:tap-vmcore-vmp-pdplight', delay: 4000 },
     { command: 'trace:start', delay: 3000 },
     { command: 'input:tap-vmcore-vmp-rec', delay: 50000 },
     { command: 'trace:stop', delay: 0 },
   ],
-  vmcore_tc21: [
+  vmcore_tc19_02: [
     { command: 'trace:start', delay: 10000 },
     { command: 'trace:stop', delay: 0 },
   ],
-  vmcore_tc22: [
+  vmcore_tc19_03: [
     { command: 'input:tap-vmcore-vmp-pdplight', delay: 4000 },
     { command: 'trace:start', delay: 3000 },
     { command: 'input:tap-vmcore-vmp-image', delay: 15000 },
