56c2e172c161efdb9126a94b147df49a4376012f
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2026-03-17 12:26:45 +0100

    chore: update url list

--- src/urls.ts
+++ src/urls.ts
@@ -1,5 +1,7 @@
 export const urls: Record<string, string> = {
   VMCORE_BYPASS: 'https://vmcore.luxottica.com/luxotticabypass',
+  VMMV_TV28_02:
+    'https://vmmv-uat.luxottica.com/v/5.6/demo/demo.html?key=95702A5E-1523-4ADD-AC16-5CE7062F7E32',
   VMMV_TV28_01:
     'https://vmmv-uat.luxottica.com/v/5.6/demo/demo.html?key=95702A5E-1523-4ADD-AC16-5CE7062F7E31',
   VMMV_TV28:
